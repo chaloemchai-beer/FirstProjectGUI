dc910a9dc6c3f79ddd8ad472801738b7be698f28
Chaloemchai Aintharaphithak <Kasutochose1@gmail.com>
2022-10-24 14:09:47 +0700

    first commit

---
+++ ProjectGUI/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int income = Convert.ToInt32(inputIncome.Text);
+            int expenses = Convert.ToInt32(inputExpenses.Text);
+            int goal = Convert.ToInt32(inputGoal.Text);
+
+            int amount = income - expenses;
+            int result = goal / amount ;
+            if(amount * result != result)
+            {
+                result = goal / amount + 1;
+            }
+            String strresult = Convert.ToString(result);
+            txtresult.Text = strresult;
+        }
+    }
+}
